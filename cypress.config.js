2879d2b50538ef0a263c2d366461f7b561544b37
Adriano Santos <eustaquio.santos.adriano@gmail.com>
2024-05-30 14:15:51 +0200

    adding config to keep only the videos from failed test cases

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,27 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
+const fs = require('fs')
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
-})
-
-module.exports = defineConfig({
+  screenshotOnRunFailure: true,
   video: true,
   e2e: {
     viewportWidth: 1920,
     viewportHeight: 1080,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('after:spec', (spec, results) => {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = results.tests.some((test) =>
+            test.attempts.some((attempt) => attempt.state === 'failed')
+          )
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            fs.unlinkSync(results.video)
+          }
+        }
+      })
     },
     baseUrl:"https://practice.expandtesting.com/notes/api",
   },
-});
+})
