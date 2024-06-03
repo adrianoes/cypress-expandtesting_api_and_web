eb64fa8924f602229049dfadbbc4bf1c43c528bd
Adriano Santos <eustaquio.santos.adriano@gmail.com>
2024-06-03 14:30:55 +0200

    code for creating account, login in and delete user was created for creating user ui test. miss other tests, hooks and custom commands for ui.

--- cypress/e2e/ui/users_ui.cy.js
+++ cypress/e2e/ui/users_ui.cy.js
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker'
+
 describe('/users_ui', () => {
 
     const baseAppUrl = `${Cypress.env('baseAppUrl')}`
@@ -6,7 +8,36 @@ describe('/users_ui', () => {
         cy.visit(baseAppUrl)  
     });
 
-    it('Check the healt of the app website', () => {
+    it('Creates a new user account', () => {
+        const user = {
+            name: faker.internet.userName(),
+            email: faker.internet.exampleEmail(),
+            password: faker.internet.password({ length: 8 })
+        }
         cy.get('[href="/notes/app/register"]').contains('Create an account').should('be.visible').click()
+        cy.get('input[name="email"]').click().type(user.email)
+        cy.get('input[name="name"]').click().type(user.name)
+        cy.get('input[name="password"]').click().type(user.password)
+        cy.get('input[name="confirmPassword"]').click().type(user.password)
+        cy.contains('button', 'Register').click()
+        cy.contains('b', 'User account created successfully').should('be.visible')
+        cy.get('[href="/notes/app/login"]').contains('Click here to Log In').should('be.visible').click()
+
+        cy.title().should('eq', 'Notes React Application for Automation Testing Practice')
+        cy.get('input[name="email"]').click().type(user.email)
+        cy.get('input[name="password"]').click().type(user.password)
+        cy.contains('button', 'Login').click()
+        cy.get('[href="/notes/app/"]').contains('Home - My Notes ').should('be.visible')
+
+        cy.get('[href="/notes/app/profile"]').contains('Profile').should('be.visible').click()
+        cy.contains('button', 'Delete Account').click()
+        cy.get('[data-testid="note-delete-confirm"]').click()
+        cy.get('[data-testid="alert-message"]').contains('Your account has been deleted. You should create a new account to continue.').should('be.visible')
+
     })
+
+
+
+
+
 })
